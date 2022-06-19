6c38c64371bab42f80bdbd8cf71da4cc7be5080a
fazt <fazt@faztweb.com>
2022-06-18 22:26:24 -0500

    added deploy to gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://faztweb.github.io/react-rick-and-morty/",
   plugins: [react(), eslint()],
 });
